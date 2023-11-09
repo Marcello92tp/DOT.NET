d16e99bbfe3e8ec1be9701aff5b844fddc3a1cb2
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-11-09 17:16:22 +0100

    esercizio 3

---
+++ esercizio3 dot net/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("inserisci il primo numero");
+string? num1 = Console.ReadLine();
+
+string? num2 = Console.ReadLine();
+
+bool conv1 = int.TryParse(num1, out int result1);
+
+bool conv2 = int.TryParse(num2, out int result2);
+int sum = result1+result2;
+
+Console.WriteLine("la somma dei due numeri è: " + sum);
