02ee8f4476ec578aa807b57453b0f7f1dbe0df0a
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-11-13 18:38:29 +0100

    esercizi .net aggiornati

---
+++ esercizio-4-dotnet/Esercizio_4_dot_net/Program.cs
@@ -0,0 +1,10 @@
+﻿
+    // Testo da invertire
+    string testoOriginale = "Marcello";
+
+    // Inverti la stringa usando Reverse e ToString
+    string testoInvertito = new string(testoOriginale.Reverse().ToString());
+
+    // Stampare il testo invertito
+    Console.WriteLine(testoInvertito);
+
