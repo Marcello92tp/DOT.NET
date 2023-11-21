a20821b98e26b9ea069df022118a446a417d3d59
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-11-21 18:32:37 +0100

    esercizio 8 inviato

---
+++ esercizio-8-dotnet/esercizio-8-dotnet/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("la parola è un palindromo?");
+
+string userInput = "pAliNdrOm e";
+string verificaPalindromo = "palindrome";
+
+Console.WriteLine($"la parola è {userInput}");
+
+string imputPulito = userInput.Replace(" ", "").ToLower();
+
+bool isPalindromo = imputPulito == verificaPalindromo;
+
+if (isPalindromo)
+{
+    Console.WriteLine("È un palindromo!");
+}
+else
+{
+    Console.WriteLine("Non è un palindromo.");
+}
