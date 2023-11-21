c64d76c58c2fec549e4a6cc94afb12fe45ac1eea
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-11-21 18:41:30 +0100

    esercizio 9 inviato

---
+++ esercizio-9-dotnet/esercizio-9-dotnet/Program.cs
@@ -0,0 +1,7 @@
+﻿
+    // Temperatura in Celsius da convertire
+    double celsius = 25.0;
+
+    // Effettua la conversione e visualizza il risultato
+    double fahrenheit = celsius * 1.8 + 32;
+    Console.WriteLine("Temperature in Fahrenheit: " + fahrenheit + "°F");
