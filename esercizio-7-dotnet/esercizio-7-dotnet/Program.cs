e1e501a1a5f915e383c9530df16c14b2435cb49f
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-11-21 18:28:40 +0100

    esercizio 7 inviato

---
+++ esercizio-7-dotnet/esercizio-7-dotnet/Program.cs
@@ -0,0 +1,11 @@
+﻿using esercizio_6_dotnet;
+Class1 myObject = new Class1();
+
+// Accesso a membri pubblici - funziona
+myObject.publcall();
+// Accesso a membri privati, protetti e interni - utilizziamo i metodi
+myObject.protCall();
+myObject.intCall();
+myObject.priCall();
+
+
