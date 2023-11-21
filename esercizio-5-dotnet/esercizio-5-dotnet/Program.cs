c6d3fe92c723461edab45f8bf39c3815050d4aed
Marcello92tp <marcellokgnoctis92@gmail.com>
2023-11-21 13:20:55 +0100

    esercizio 5 completato

---
+++ esercizio-5-dotnet/esercizio-5-dotnet/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Write("Inserisci il percorso del file: ");
+string filePath = Console.ReadLine();
+
+// Verifica se il percorso è assoluto o relativo
+if (!Path.IsPathRooted(filePath))
+{
+    // verifica percorso assoluto
+    filePath = Path.GetFullPath(filePath);
+}
+// se il file esiste
+if (File.Exists(filePath))
+{
+    // prendo tutto il contenuto del file e invio a schermo
+    string content = File.ReadAllText(filePath);
+    Console.WriteLine("Contenuto del file:");
+    Console.WriteLine(content);
+}
+else
+{
+    Console.WriteLine("Il file specificato non esiste.");
+}
+    
+
